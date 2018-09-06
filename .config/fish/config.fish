31ac1798c34d9a3ec60a41770d03956d5f264423
ippachi <ivalice1218@gmail.com>
2018-09-06 11:45:26 +0900

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+set -x PATH $HOME/.rbenv/bin $PATH
+
+rbenv init - | source
+
+set -x PATH $HOME/.rbenv/plugins/ruby-build/bin $PATH
+
+alias be='bundle exec'
+alias bers='bundle exec rails s -b 0.0.0.0'
+alias behs='bundle exec hanami s --host 0.0.0.0 --port 3000'
+alias bet='env RAILS_ENV=test bundle exec'
+alias ch='cd $PROJECT_HOME'
+alias dc='docker-compose'
+alias ce='composer exec'
+alias nvimf='nvim (fzf)'
+alias chc='sudo sysctl -w vm.drop_caches=3'
+alias lbe='env BUNDLE_GEMFILE="Gemfile.local" bundle exec'
+
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
+set -x PATH /usr/local/go/bin $PATH
+set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
+
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x FZF_DEFAULT_OPTS '--height 40% --reverse --border'
+set -x FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
+
+if command -v pyenv 1>/dev/null 2>&1;
+  pyenv init - | source
+end
+
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
+
+set -x EDITOR nvim
+set -g theme_nerd_fonts yes
+
+direnv hook fish | source
