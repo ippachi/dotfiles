9c5fc4bb90ae618716f48021a5a3e46fcdf0930b
ippachi <ippachi1018@gmail.com>
2023-12-12 09:30:34 +0900

    Update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,6 +5,9 @@ local augroup = api.nvim_create_augroup("my-vimrc", { clear = true })
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
@@ -27,14 +30,13 @@ vim.opt.title = true
 vim.opt.undofile = true
 vim.opt.mouse = ""
 vim.opt.formatoptions:append({
-  t = true,
-  m = true,
-  M = true,
+	t = true,
+	m = true,
+	M = true,
 })
 vim.opt.diffopt = { "internal", "filler", "algorithm:histogram", "indent-heuristic", "linematch:60" }
 vim.opt.updatetime = 300
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
-vim.cmd([[set efm+=%f\|%l\ col\ %c\|%m]])
 
 vim.cmd([[packadd Cfilter]])
 
@@ -45,228 +47,189 @@ keymap.set("n", "k", "gk", { noremap = true })
 keymap.set("t", "<c-o>", "<c-\\><c-n>", { noremap = true })
 
 vim.api.nvim_create_autocmd("QuickFixcmdPost", {
-  group = augroup,
-  pattern = { "grep", "vimgrep" },
-  callback = function()
-    vim.cmd([[cwindow]])
-  end,
+	group = augroup,
+	pattern = { "grep", "vimgrep" },
+	callback = function()
+		vim.cmd([[cwindow]])
+	end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme gruvbox-material]])
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme gruvbox-material]])
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
 
-          map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-          map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-          map("n", "<leader>hS", gs.stage_buffer)
-          map("n", "<leader>hu", gs.undo_stage_hunk)
-          map("n", "<leader>hR", gs.reset_buffer)
-          map("n", "<leader>hp", gs.preview_hunk)
-          map("n", "<leader>hb", function()
-            gs.blame_line({ full = true })
-          end)
-          map("n", "<leader>tb", gs.toggle_current_line_blame)
-          map("n", "<leader>hd", gs.diffthis)
-          map("n", "<leader>hD", function()
-            gs.diffthis("~")
-          end)
-          map("n", "<leader>td", gs.toggle_deleted)
-        end,
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "RRethy/nvim-treesitter-endwise",
-      "nvim-treesitter/playground",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-          disable = { "embedded_template" },
-        },
-        endwise = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-            },
-          },
-        },
-      })
-    end,
-  },
-  { "itchyny/vim-qfedit" },
-  {
-    "neoclide/coc.nvim",
-    branch = "release",
-    config = function()
-      local keymap = vim.keymap
+					map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+					map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+					map("n", "<leader>hS", gs.stage_buffer)
+					map("n", "<leader>hu", gs.undo_stage_hunk)
+					map("n", "<leader>hR", gs.reset_buffer)
+					map("n", "<leader>hp", gs.preview_hunk)
+					map("n", "<leader>hb", function()
+						gs.blame_line({ full = true })
+					end)
+					map("n", "<leader>tb", gs.toggle_current_line_blame)
+					map("n", "<leader>hd", gs.diffthis)
+					map("n", "<leader>hD", function()
+						gs.diffthis("~")
+					end)
+					map("n", "<leader>td", gs.toggle_deleted)
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"RRethy/nvim-treesitter-endwise",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+					disable = { "embedded_template" },
+				},
+				endwise = { enable = true },
+			})
+		end,
+	},
+	{ "itchyny/vim-qfedit" },
+	{
+		"echasnovski/mini.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		version = false,
+		config = function()
+			require("mini.pairs").setup()
+			require("mini.comment").setup()
+			require("mini.surround").setup()
+			require("mini.trailspace").setup()
+			require("mini.statusline").setup()
+			require("mini.files").setup()
+			require("mini.ai").setup()
+			require("mini.hipatterns").setup()
+			require("mini.indentscope").setup()
+			require("mini.extra").setup()
+			require("mini.completion").setup()
+			require("mini.pick").setup({
+				mappings = {
+					choose_marked = "<C-q>",
+				},
+			})
 
-      -- Use <c-l> to trigger completion
-      keymap.set("i", "<c-l>", "coc#refresh()", { silent = true, expr = true })
+			vim.keymap.set("n", "<c-p>", function()
+				MiniPick.builtin.files()
+			end, { noremap = true })
+			vim.keymap.set("n", "<c-h>", function()
+				MiniPick.builtin.help()
+			end, { noremap = true })
 
-      -- GoTo code navigation
-      keymap.set("n", "gd", "<Plug>(coc-definition)", { silent = true })
-      keymap.set("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
-      keymap.set("n", "gi", "<Plug>(coc-implementation)", { silent = true })
-      keymap.set("n", "gr", "<Plug>(coc-references)", { silent = true })
+			vim.keymap.set("n", "<space>f", function()
+				MiniFiles.open(vim.api.nvim_buf_get_name(0))
+			end, { noremap = true })
+		end,
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
 
-      -- Use K to show documentation in preview window
-      function _G.show_docs()
-        local cw = vim.fn.expand('<cword>')
-        if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
-          vim.api.nvim_command('h ' .. cw)
-        elseif vim.api.nvim_eval('coc#rpc#ready()') then
-          vim.fn.CocActionAsync('doHover')
-        else
-          vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-        end
-      end
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
 
-      keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
+				["ruby_ls"] = function()
+					require("lspconfig")["ruby_ls"].setup({
+						init_options = {
+							formatter = false,
+							featuresConfiguration = {
+								inlayHint = {
+									enableAll = true,
+								},
+							},
+						},
+					})
+				end,
+			})
 
-      -- Highlight the symbol and its references on a CursorHold event(cursor is idle)
-      vim.api.nvim_create_augroup("CocGroup", {})
-      vim.api.nvim_create_autocmd("CursorHold", {
-        group = "CocGroup",
-        command = "silent call CocActionAsync('highlight')",
-        desc = "Highlight symbol under cursor on CursorHold"
-      })
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-      -- Symbol renaming
-      keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
-
-      -- Formatting selected code
-      keymap.set("x", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
-      keymap.set("n", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
-
-      -- Setup formatexpr specified filetype(s)
-      vim.api.nvim_create_autocmd("FileType", {
-        group = "CocGroup",
-        pattern = "typescript,json",
-        command = "setl formatexpr=CocAction('formatSelected')",
-        desc = "Setup formatexpr specified filetype(s)."
-      })
-
-      -- Update signature help on jump placeholder
-      vim.api.nvim_create_autocmd("User", {
-        group = "CocGroup",
-        pattern = "CocJumpPlaceholder",
-        command = "call CocActionAsync('showSignatureHelp')",
-        desc = "Update signature help on jump placeholder"
-      })
-
-      -- Apply codeAction to the selected region
-      -- Example: `<leader>aap` for current paragraph
-      local opts = { silent = true, nowait = true }
-      keymap.set("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
-      keymap.set("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
-
-      -- Remap keys for apply code actions at the cursor position.
-      keymap.set("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
-
-      -- Remap keys for apply source code actions for current file.
-      keymap.set("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts)
-
-      -- Apply the most preferred quickfix action on the current line.
-      keymap.set("n", "<leader>qf", "<Plug>(coc-fix-current)", opts)
-
-      -- Remap keys for apply refactor code actions.
-      keymap.set("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
-      keymap.set("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
-      keymap.set("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
-
-      -- Run the Code Lens actions on the current line
-      keymap.set("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts)
-
-      local opts = { silent = true, nowait = true, expr = true }
-      keymap.set("n", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
-      keymap.set("n", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
-      keymap.set("i", "<C-f>",
-        'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', opts)
-      keymap.set("i", "<C-b>",
-        'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts)
-      keymap.set("v", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
-      keymap.set("v", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
-
-      -- Add `:Format` command to format current buffer
-      vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
-
-      -- " Add `:Fold` command to fold current buffer
-      vim.api.nvim_create_user_command("Fold", "call CocAction('fold', <f-args>)", { nargs = '?' })
-
-      -- Add `:OR` command for organize imports of the current buffer
-      vim.api.nvim_create_user_command("OR", "call CocActionAsync('runCommand', 'editor.action.organizeImport')", {})
-    end
-  },
-  {
-    'echasnovski/mini.nvim',
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    },
-    version = false,
-    config = function()
-      require('mini.pairs').setup()
-      require('mini.comment').setup()
-      require('mini.surround').setup()
-      require('mini.trailspace').setup()
-      require('mini.statusline').setup()
-      require('mini.files').setup()
-      require('mini.ai').setup()
-      require('mini.hipatterns').setup()
-      require('mini.indentscope').setup()
-      require('mini.pick').setup({
-        mappings = {
-          choose_marked = '<C-q>'
-        }
-      })
-
-      vim.keymap.set("n", "<c-p>", function() MiniPick.builtin.files() end, { noremap = true })
-      vim.keymap.set("n", "<c-h>", function() MiniPick.builtin.help() end, { noremap = true })
-
-      vim.keymap.set("n", "<space>f", function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end, { noremap = true })
-    end
-  },
-  { "koron/vim-budoux" },
-
-  { "tpope/vim-fugitive", cmd = "Git" },
-  { "github/copilot.vim", cmd = "Copilot", event = "InsertEnter" }
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>f", function()
+						vim.lsp.buf.format({ async = true })
+					end, opts)
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					typescriptreact = { { "prettierd", "prettier" } },
+					typescript = { { "prettierd", "prettier" } },
+				},
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+			})
+		end,
+	},
 })
