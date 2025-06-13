b0e2baa4a94924ce10e7449f8680f1ab9bdcf60f
ippachi <ippachi1018@gmail.com>
2025-06-14 01:39:14 +0900

    Update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,4 +3,7 @@ if status is-interactive
 
   # Commands to run in interactive sessions can go here
   /opt/homebrew/bin/brew shellenv | source
+
+  # fish-done
+  set -U __done_notification_command "echo -e \"\033]777;notify;\$title;\$message\007\""
 end
